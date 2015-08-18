cc64ff6a8dda0bb2b45a8487795c61298f4502e2
TommyLi <tommyli76278@gmail.com>
2015-08-18 09:29:18 -0700

    I deleted the <> thing.

--- Tommy_Li_Project1/Hello_World.java
+++ Tommy_Li_Project1/Hello_World.java
@@ -1,15 +1,15 @@
 
 /**
- * This program prints Hello World in the serial monitor
+ * This program prints Hello World! in the serial monitor
  * 
- * Tommy Li
- * 001 08/17/15
+ * @author Tommy Li
+ * @versuib 08/17/15
  */
 public class Hello_World
 {
   public static void main(String[] args)
   {
-      System.out.println("Hello World! From <Tommy Li>");
+      System.out.println("Hello World! From Tommy Li.");
     //Display the string
     }
 }
