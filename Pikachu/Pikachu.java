642060a31927dc8d804840973a65218a18a6a610
TommyLi <tommyli76278@gmail.com>
2015-08-21 08:38:42 -0700

    My Pikachu code

---
+++ Pikachu/Pikachu.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Pikachu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pikachu
+{
+  public static void main(String[] args)
+  {   
+      System.out.println("Pikachu welcomes you to the world of Pokemon!");
+      System.out.println("\t\t (\\__/)");
+      System.out.println("\t\t (o^.^)");
+      System.out.println("\t\tz(_(\")(\")");
+    }
+}
