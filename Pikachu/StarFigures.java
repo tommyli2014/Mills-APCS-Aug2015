429c1184017f0ca104b2ca19a68017a36d711808
TommyLi <tommyli76278@gmail.com>
2015-08-25 08:58:23 -0700

    I added StarFigures file

---
+++ Pikachu/StarFigures.java
@@ -0,0 +1,47 @@
+
+/**
+ * Write a description of class StarFigures here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StarFigures
+{
+  public static void main(String[] args)
+  {doubleStar();
+   System.out.println();
+   fiveStar();
+   System.out.println();
+   doubleStar();
+   System.out.println();
+   fiveStar();
+   System.out.println();
+   doubleStar();
+   System.out.println();
+   System.out.println();
+   threeStar();
+   System.out.println();
+   doubleStar();
+   System.out.println();
+   fiveStar();
+}
+public static void doubleStar() {
+    System.out.println("*****");
+    System.out.println("");
+    System.out.println("*****");
+}
+public static void fiveStar() {
+    System.out.println(" * * ");
+    System.out.println("");
+    System.out.println("  *  ");
+    System.out.println();
+    System.out.println(" * * ");
+}
+public static void threeStar(){
+    System.out.println("  *  ");
+    System.out.println();
+    System.out.println("  *  ");
+    System.out.println();
+    System.out.println("  *  ");
+}
+}
